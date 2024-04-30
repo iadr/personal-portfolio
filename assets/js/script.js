3f3157fec109494bb8509dedc3fc455764ef636d
Israel Díaz <israel.diazrosas@outlook.com>
2024-04-29 22:22:24 -0400

    actualizado curriculum

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,6 +100,8 @@ for (let i = 0; i < formInputs.length; i++) {
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
+console.log(pages);
+console.log(navigationLinks); 
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
